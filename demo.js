ebb31e8717667b88792f32f439ed2398bdcd0396
siven.jin <siven.jin@qunar.com>
2018-08-14 15:18:53 +0800

    parallel
    parallel

--- demo.js
+++ demo.js
@@ -1,130 +1,99 @@
 import React, { Component } from 'react'
 import {
-    StyleSheet,
-    View,
-    Image,
-    Dimensions,
-    Platform,
-    Animated
-  } from 'react-native';
-import HomeHeader from "./header";
-const deviceWidth = Dimensions.get("window").width;
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Animated,
+  View,
+  Easing,
+  TouchableHighlight,
+  Dimensions
+} from 'react-native'
 
-export default class Home extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            scrollY:new Animated.Value(0)
-        };
-    }
-    componentDidMount(){
-    }
-    render() {
-        const {
-            scrollY
-        } =this.state;
-        const imageOpacity = scrollY.interpolate({
-            inputRange: [0,10,20,30,40],
-            outputRange: [0,0.1,0.2,0.5,1],
-            extrapolate: 'clamp'
-          });
-          const backgroundTranslate = scrollY.interpolate({
-            inputRange: [-1000,-600,-500,-400,-300,0, 100,4000],
-            outputRange:[-1000,-600,-500,-400,-300,-500,-600,-4000],
-            extrapolate: 'clamp'
-          });
-        return (
-            <View style={styles.fill}>
-                <HomeHeader 
-                    // style={{backgroundColor:"#25DAF4"}}
-                >
-                </HomeHeader>
-                <Animated.View style={[styles.bluebackground,{ transform: [{ translateY: backgroundTranslate}] }]}/>
-                <View 
-                style={styles.fullScreen}
-                >   
-                <Animated.View style={[styles.stickyHeader,{opacity:imageOpacity}]}>
-                    <View style={[styles.stickyHeaderInner]}></View>
-                </Animated.View>
-                <Animated.ScrollView 
-                style={styles.container}
-                onScroll = {Animated.event(
-                    [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
-                    {useNativeDriver: true}
-                  )}
-                    scrollEventThrottle={1}
-                >
-                    
-                    <Image
-                        resizeMode={"stretch"}
-                        source={require("./myHeader.jpg")} 
-                        style={styles.headerImg}
-                        > 
-                    </Image>                    
-                    <View style={styles.content}>
-                        
-                        
-                    </View>
-                    <View style={styles.footer}>
+const { width } = Dimensions.get('window');
 
-                    </View>
-                </Animated.ScrollView>
-            </View>
-            
-            </View>
-        )
+class Animations extends Component {
+
+  constructor () {
+    super()
+    this.animatedValue1 = new Animated.Value(0)
+    this.animatedValue2 = new Animated.Value(0)
+    this.animatedValue3 = new Animated.Value(0)
+  }
+
+  componentDidMount () {
+    this.animate()
+  }
+
+  animate () {
+    this.animatedValue1.setValue(0)
+    this.animatedValue2.setValue(0)
+    this.animatedValue3.setValue(0)
+    const createAnimation = function (value, duration, easing, delay = 0) {
+      return Animated.timing(
+       value,
+        {
+          toValue: 1,
+          duration,
+          easing,
+          delay
+        }
+      )
     }
+    Animated.parallel([
+      createAnimation(this.animatedValue1, 2000, Easing.ease),
+      createAnimation(this.animatedValue2, 1000, Easing.ease, 1000),
+      createAnimation(this.animatedValue3, 1000, Easing.ease, 2000)
+    ]).start()
+  }
+
+  render () {
+    const scaleText = this.animatedValue1.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0.5, 2]
+    })
+    const spinText = this.animatedValue2.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '720deg']
+    })
+    const introButton = this.animatedValue3.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-100, 400]
+    })
+    return (
+      <View style={[styles.container]}>
+        <Animated.View style={{ transform: [{scale: scaleText}] }}>
+          <Text>Welcome</Text>
+        </Animated.View>
+        <Animated.View style={{ marginTop: 20, transform: [{rotate: spinText}] }}>
+          <Text style={{fontSize: 20}}>to the App!</Text>
+        </Animated.View>
+        <Animated.View style={{top: introButton, position: 'absolute'}}>
+          <TouchableHighlight onPress={this.animate.bind(this)} style={styles.button}>
+            <Text style={{color: 'white', fontSize: 20}}>Click Here To Start</Text>
+          </TouchableHighlight>
+        </Animated.View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    fill:{
-        flex: 1,
-        position:"relative",
-        backgroundColor:"#f5f5f5"
-    },
-    fullScreen:{
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        flex:1
-        
-    },
-    bluebackground:{
-        height:600,
-        backgroundColor:"#25DAF4"
-    
-    },
-    stickyHeader:{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 2,
-        borderWidth:0
-    },
-    stickyHeaderInner:{
-        height:  Platform.OS === "iOS" ? (isIphoneX ? 88 : 64) :56,
-        backgroundColor: '#00DAF6'
-    },
-    container: {
-      flex: 1
-    },
-    
-    headerImg:{
-        height:132,
-        width:deviceWidth,
-        zIndex:4
-    },
-    content:{
-        flex: 1,
-        backgroundColor:"#fff",
-        paddingTop:20,
-        height:1000
-    },
-    footer:{
-        height:50,
-        backgroundColor:"#f5f5f5"
-    }
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    width: width - 40,
+    height: 70,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+
+export default Animations;
