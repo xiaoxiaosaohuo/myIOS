97686331464edec2c5dc565a056c81caebba8ed0
金 鑫 <jinxin@hualala.com>
2018-11-08 20:53:47 +0800

    d

--- data.js
+++ data.js
@@ -3,21 +3,4 @@ export default [
     image: "http://pic1.win4000.com/wallpaper/0/58ef08f710c56.jpg",
     title: "Le Brûloir"
   },
-  {
-    image: "http://pic1.win4000.com/wallpaper/0/58ef08fe74c35.jpg",
-    title: "Le Petit Brûloir"
-  },
-  {
-    image:
-      "http://2.bp.blogspot.com/-Cu3p8pH_N88/VCb9YTXfywI/AAAAAAAAQwI/69VfoVHgE-E/s1600/hinh-nen-mua-thu-dep-va-quyen-ru-nhat-1.jpg",
-    title: "Oui Mais Non"
-  },
-  {
-    image: "http://pic1.win4000.com/wallpaper/0/58ef08ec54bf3.jpg",
-    title: "PERKO"
-  },
-  {
-    image: "http://pic1.win4000.com/wallpaper/0/58ef08fc118bf.jpg?down",
-    title: "Perko"
-  }
 ];
